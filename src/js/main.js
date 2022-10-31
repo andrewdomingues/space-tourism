f45e8c0da851c69a57aff28c9861ff62c061ae52
Andrew Domingues <andrewluisdomingues@protonmail.com>
2022-10-31 08:09:29 -0400

    destinations pages done!

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,17 @@ const hamMenuIconOpen = document.querySelector("#ham-menu-icon-open")
 const hamMenuIconClose = document.querySelector("#ham-menu-icon-close")
 const menu = document.querySelector("#menu")
 
-hamMenuIconOpen.addEventListener("click", () => {
+function hamMenuOpen(){
   menu.classList.remove("menu-close")
   menu.classList.add("menu-open")
   hamMenuIconOpen.classList.add("hide")
   hamMenuIconClose.classList.remove("hide")
-})
-hamMenuIconClose.addEventListener("click", () => {
+}
+function hamMenuClose(){
   hamMenuIconClose.classList.add("hide")
   hamMenuIconOpen.classList.remove("hide")
   menu.classList.remove("menu-open")
   menu.classList.add("menu-close")
-})
+}
+hamMenuIconOpen.addEventListener("click", hamMenuOpen)
+hamMenuIconClose.addEventListener("click", hamMenuClose)
