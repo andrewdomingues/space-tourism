1d7a2273832d0c330fa6f471c633494d973920f2
Andrew Domingues <andrewluisdomingues@protonmail.com>
2022-10-31 03:49:05 -0400

    add responsive background image

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
-const hamMenu = document.querySelector('#ham-menu')
-const hamMenuIconOpen = document.querySelector('#ham-menu-icon-open')
-const hamMenuIconClose = document.querySelector('#ham-menu-icon-close')
-const menu = document.querySelector('#menu')
+const hamMenu = document.querySelector("#ham-menu")
+const hamMenuIconOpen = document.querySelector("#ham-menu-icon-open")
+const hamMenuIconClose = document.querySelector("#ham-menu-icon-close")
+const menu = document.querySelector("#menu")
 
-hamMenuIconOpen.addEventListener('click', () => {
-  menu.classList.remove('menu-close')
-  menu.classList.add('menu-open')
-  hamMenuIconOpen.classList.add('hide')
-  hamMenuIconClose.classList.remove('hide')
+hamMenuIconOpen.addEventListener("click", () => {
+  menu.classList.remove("menu-close")
+  menu.classList.add("menu-open")
+  hamMenuIconOpen.classList.add("hide")
+  hamMenuIconClose.classList.remove("hide")
 })
-hamMenuIconClose.addEventListener('click', () => {
-  hamMenuIconClose.classList.add('hide')
-  hamMenuIconOpen.classList.remove('hide')
-  menu.classList.remove('menu-open')
-  menu.classList.add('menu-close')
+hamMenuIconClose.addEventListener("click", () => {
+  hamMenuIconClose.classList.add("hide")
+  hamMenuIconOpen.classList.remove("hide")
+  menu.classList.remove("menu-open")
+  menu.classList.add("menu-close")
 })
